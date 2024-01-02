cb217d0304c24fe8ab1005b828267a0c69b5a34f
Marcos Rogerio <marcosmiyamotoshigeru@gmail.com>
2024-01-01 21:33:11 -0300

    Automacao com Selenium

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("Estamos fazendo nosso primeiro projeto")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def iniciar_driver():
+    chrome_options = Options()
+    arguments =['--lang=pt-BR', '--window-size=800,600', '--icognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1,
+    })
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()),options=chrome_options)
+    
+    return driver
+
+driver = iniciar_driver()
+driver.get("https://www.youtube.com/")
+input("Aperte alguma tecla para fechar")
